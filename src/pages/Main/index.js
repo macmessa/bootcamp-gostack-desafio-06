0d723be8138afec41521cd659f80264ce67fac12
macmessa <mac.messa@gmail.com>
2019-08-24 23:09:14 -0300

    fix(Main): validate user profile

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Keyboard, ActivityIndicator } from 'react-native';
+import { Keyboard, ActivityIndicator, Alert } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -57,22 +57,27 @@ export default class Main extends Component {
 
     this.setState({ loading: true });
 
-    const response = await api.get(`/users/${newUser}`);
+    if (newUser) {
+      try {
+        const response = await api.get(`/users/${newUser}`);
 
-    const data = {
-      name: response.data.name,
-      login: response.data.login,
-      bio: response.data.bio,
-      avatar: response.data.avatar_url,
-    };
+        const data = {
+          name: response.data.name,
+          login: response.data.login,
+          bio: response.data.bio,
+          avatar: response.data.avatar_url,
+        };
 
-    this.setState({
-      newUser: '',
-      users: [...users, data],
-      loading: false,
-    });
+        this.setState({
+          users: [...users, data],
+        });
+      } catch (error) {
+        Alert.alert('', 'Usuário não encontrado', [{ text: 'OK' }]);
+      }
+    }
 
     Keyboard.dismiss();
+    this.setState({ newUser: '', loading: false });
   };
 
   handleNavigate = user => {
